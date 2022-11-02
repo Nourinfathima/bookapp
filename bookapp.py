fe1265abcc54050dcb82a678852b26c683ed6879
nourinfathima <nourinnourin36@gmail.com>
2022-11-03 02:21:21 +0530

    view

--- bookapp.py
+++ bookapp.py
@@ -24,6 +24,12 @@ while True:
         mydb.commit()
     elif(choice == 2):
         print('view all book selected')
+        sql = 'SELECT * FROM `books`'
+        mycursor.execute(sql)
+        result = mycursor.fetchall()
+        for i in result:
+            print(i)
+        print('Sucessfully !!!!')
     elif(choice==3):
         print('search a book selected')
     elif(choice==4):
