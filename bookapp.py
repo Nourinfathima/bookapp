36c3b14ade5f3231591db5b5adf24a520bf0f381
nourinfathima <nourinnourin36@gmail.com>
2022-11-03 02:16:36 +0530

    option

--- bookapp.py
+++ bookapp.py
@@ -1,3 +1,7 @@
+import mysql.connector
+mydb = mysql.connector.connect(host = 'localhost' , user = 'root' , password = '' , database = 'librarydb')
+mycursor = mydb.cursor()
+
 while True:
     print("select an option from the menu")
     print('1 add book')
@@ -10,6 +14,14 @@ while True:
     choice = int(input('Enter an option: '))
     if(choice == 1):
         print('Book enter selected')
+        book_name = input('Enter the book name : ')
+        bookauthor = input('Enter the author of the book : ')
+        bookcategory = input('Enter the category for each day : ')
+        book_rentprice = input('Enter the price  : ')
+        sql = 'INSERT INTO `books`(`bookname`, `bookauthor`, `bookcategory`, `book_rentprice`) VALUES(%s,%s,%s,%s) '
+        data = (book_name,bookauthor,bookcategory,book_rentprice)
+        mycursor.execute(sql,data)
+        mydb.commit()
     elif(choice == 2):
         print('view all book selected')
     elif(choice==3):
